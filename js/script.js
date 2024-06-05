7dab6222ac8335d23288d964f4e25acbe403b162
Aniana <anigm89@gmail.com>
2024-06-05 13:14:14 +0200

    corregido

--- js/script.js
+++ js/script.js
@@ -1,100 +1,47 @@
 import peliculas from './peliculas.js'
 //console.log(peliculas);
 
-const accion = document.getElementById('genero-28');
-const thriller = document.getElementById('genero-53');
-const aventura = document.getElementById('genero-12');
+const filtradopeliculas = (generoId, contenedorId) =>
+{
+    const contenedor = document.getElementById(contenedorId);   
+    const peliculasgenero = peliculas.filter((pelicula) => pelicula.genre_ids.includes(generoId)); console.log(peliculasgenero)
 
-
- let generos = []; 
- let titulos = [];
-
- function separar() {
-    for(let i=0; i<peliculas.length;i++) {
-            generos.push(peliculas[i].genre_ids);
-            titulos.push(peliculas[i].title);
+    for(const pelicula of peliculasgenero)
+    {
+        let templatepelis = ` 
+        <div class = "pelicula">
+            <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt ="${pelicula.title}">
+            <p>${pelicula.title}</p>
+        </div> `
+        contenedor.innerHTML += templatepelis;
     }
-   // return generos;
-    //console.log('generos ' + generos + ' pelis ' + titulos);
-    
 }
-separar(peliculas);
-//console.log('generos ',generos);
-//console.log(titulos);
 
+filtradopeliculas(28,'genero-28');
+filtradopeliculas(53,'genero-53');
+filtradopeliculas(12,'genero-12');
+
+
+
+///lo mismo pero con foreach
 /*
-let generoaccion;
-let generothriller;
-let generoaventuras;
-generos.forEach(genero => {
-       console.log('gen', genero);
-    const generoaccion1 = genero.filter((peli) => peli === 28);       console.log('accion', generoaccion1); 
-    const generotrhiller1 = genero.filter((peli) => peli === 53);       console.log('thriller', generotrhiller1); 
-    const generoaventuras1 = genero.find((num) => num === 12);       console.log('aventuras', generoaventuras1); 
-    generoaccion = generoaccion1;
-    generothriller = generotrhiller1;
-    generoaventuras = generoaventuras1;
+const filtradopeliculas = (generoId, contenedorId) =>
+{
+    const contenedor = document.getElementById(contenedorId);
+    const peliculasgenero = peliculas.filter((pelicula) => pelicula.genre_ids.includes(generoId));
 
-});
+    peliculasgenero.forEach((pelicula) => 
+    {
+        let templatepelis = ` 
+        <div class = "pelicula">
+            <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt ="${pelicula.title}">
+            <p>${pelicula.title}</p>
+        </div> `
+        contenedor.innerHTML += templatepelis;
+    })
+}
 
- console.log('no', generoaventuras)
- */
-
- //const generosSeleccionados = [28, 53,12];
- const generoaventuras = [12]
- const generothriller= [53];
- const generoaccion = [28];
-
- // Filtrar las películas por género aventuras
- const peliculasFiltradasadv = peliculas.filter(pelicula =>
-     pelicula.genre_ids.some(id => generoaventuras.includes(id))
-   );
-   
-   //console.log('x', peliculasFiltradas)
-
-   for(let i=0; i<peliculasFiltradasadv.length;i++) 
-   {
-    let  pelisaventuras = peliculasFiltradasadv[i].title;
-    let fotosaventuras =peliculasFiltradasadv[i].poster_path;
-     console.log('z',pelisaventuras);
- 
-         aventura.innerHTML += 
-         `<div><p> ${pelisaventuras } </p>
-         <img src="https://image.tmdb.org/t/p/w500${fotosaventuras}"></div>` ;
-     
-   }
-
-
-// Filtrar las películas por género thriller
-const peliculasFiltradasT = peliculas.filter(pelicula =>
-    pelicula.genre_ids.some(id => generothriller.includes(id))
-  );
-   for(let i=0; i<peliculasFiltradasT.length;i++) 
-  {
-   let  pelisthriller = peliculasFiltradasT[i].title;
-   let cartelthriller =peliculasFiltradasT[i].poster_path;
-    //console.log('z',generothriller);
-
-        thriller.innerHTML += 
-        `<div><p> ${pelisthriller } </p>
-        <img src="https://image.tmdb.org/t/p/w500${cartelthriller}"></div>` ;
-
-  }
-
-  // Filtrar las películas por género accion
-const peliculasFiltradasACC = peliculas.filter(pelicula =>
-    pelicula.genre_ids.some(id => generoaccion.includes(id))
-  );
-   for(let i=0; i<peliculasFiltradasACC.length;i++) 
-  {
-   let  pelisaccion = peliculasFiltradasACC[i].title;
-   let cartelaccion =peliculasFiltradasACC[i].poster_path;
-    //console.log('z',generothriller);
-
-        accion.innerHTML += 
-        ` <div>${pelisaccion } 
-        <img src="https://image.tmdb.org/t/p/w500${cartelaccion}"></div>` ;
-    
-  }
-  
-
+filtradopeliculas(28,'genero-28');
+filtradopeliculas(53,'genero-53');
+filtradopeliculas(12,'genero-12');
+*/
